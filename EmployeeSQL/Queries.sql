9c1baca2a532f1ede55bfe8cdc4af1835bc3a4a3
Brittanie <fraserbrittanie@gmail.com>
2022-09-18 21:16:21 -0700

    working changes for schema

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -16,14 +16,20 @@ order by hire_date ASC;
 
 --List the manager of each department with the following information: 
 --department number, department name, the manager's employee number, last name, first name.
- 
-
+select d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+from employees as e
+inner join dept_manager as dm on dm.emp_no=e.emp_no
+inner join departments as d on d.dept_no=dm.dept_no
+order by dept_no ASC;
 
 
 --List the department of each employee with the following information: 
 --employee number, last name, first name, and department name.
-
-
+select e.emp_no, e.last_name, e.first_name, dp.dept_name
+from employees as e
+left join dept_emp as d on e.emp_no=d.emp_no
+left join departments as dp on d.dept_no=dp.dept_no
+order by last_name ASC;
 
 
 --List first name, last name, and sex for employees whose first name is
@@ -36,13 +42,26 @@ order by last_name ASC;
 
 --List all employees in the Sales department, including their 
 --employee number, last name, first name, and department name.
+select e.emp_no, e.last_name, e.first_name, dp.dept_name
+from employees as e
+left join dept_emp as d on e.emp_no=d.emp_no
+left join departments as dp on d.dept_no=dp.dept_no
+where dp.dept_name = 'Sales';
 
 
-
---List all employees in the Sales and Development departments, 
---including their employee number, last name, first name, and department name.
-
+--List all employees in the Sales and Development departments, including their 
+--employee number, last name, first name, and department name.
+select e.emp_no, e.last_name, e.first_name, dp.dept_name
+from employees as e
+left join dept_emp as d on e.emp_no=d.emp_no
+left join departments as dp on d.dept_no=dp.dept_no
+where dp.dept_name = 'Sales' OR dp.dept_name = 'Development';
 
 
 --List the frequency count of employee last names 
 --(i.e., how many employees share each last name) in descending order.
+select last_name, count(*) as "Last Name Count"
+from employees
+group by last_name
+order by "Last Name Count" DESC;
+
